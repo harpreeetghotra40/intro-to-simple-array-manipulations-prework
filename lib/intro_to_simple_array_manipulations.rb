48a7fa4bc3710b8972c37eb700c98ebdeac210ae
HARPREET GHOTRA <harpreetghotra40@icloud.com>
2019-04-15 20:39:55 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -7,3 +7,32 @@ def using_unshift(array, name)
   array.unshift(name)
   return array
 end
+
+def using_pop(array)
+  array.pop
+  return array
+end
+
+def pop_with_args(array , num)
+  while num > 0
+    array.pop
+  end
+  return array
+end
+
+def using_shift(array)
+  array.shift
+  return array
+end
+
+def shift_with_args(array , num)
+  while num > 0
+    array.shift
+  end
+  return array
+end
+
+def using_concat(array1 , array2)
+  array1.concat(array2)
+  return array1
+end
